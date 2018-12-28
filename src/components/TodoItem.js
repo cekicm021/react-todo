d7d25274f437fa6116951cf7819a0d1208800f59
Marko Cekic <cekicm021@gmail.com>
2018-12-28 20:33:19 +0100

    final push

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 
 export default class TodoItem extends Component {
-    render() {
-        const { title, handleDelete } = this.props
-        return (
-            <li className="list-group-item text-capitalizze d-flex justify-content-between my-2">
-                <h6>{title}</h6>
-                <div className="todo-icon">
-                    <span className="mx-2 text-success"><i className="fas fa-pen"></i></span>
-                    <span className="mx-2 text-danger" onClick={handleDelete}><i className="fas fa-trash"></i></span>
-                </div>
-            </li>
-        );
-    }
+	render () {
+		const { title, handleDelete, handleEdit } = this.props;
+		return (
+			<li className="list-group-item text-capitalizze d-flex justify-content-between my-2">
+				<h6>{title}</h6>
+				<div className="todo-icon">
+					<span className="mx-2 text-success" onClick={handleEdit}><i className="fas fa-pen"></i></span>
+					<span className="mx-2 text-danger" onClick={handleDelete}><i className="fas fa-trash"></i></span>
+				</div>
+			</li>
+		);
+	}
 }
